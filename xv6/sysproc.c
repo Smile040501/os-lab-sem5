baa6a01552f32bf391b0b374f2899ca2d45f8954
Mayank Singla <singlamayank001@gmail.com>
2021-11-14 18:26:06 +0530

    Implemented initial version of v2paddr syscall

--- xv6/sysproc.c
+++ xv6/sysproc.c
@@ -85,15 +85,57 @@ int sys_getcount(void) {
     return myproc()->countSyscalls[syscall_num - 1];
 }
 
+// System call to convert a virtual address to physical address
+/*
+    Takes two input arguments
+        paddr - an unsigned integer pointer to store the final answer
+        vaddr - an unsigned integer pointer which is the virtual address to convert
+*/
+// Returns -1 if input vaddr is invalid or there is any fault in page table walk and vaddr should be accessible in user mode
 int sys_v2paddr(void) {
-    addr_t *vaddr, *paddr;
-    if(argptr(0, (void *) &paddr, sizeof(*paddr)) < 0) {
+    addr_t *paddr, *vaddr;
+    int paddr_num = 0, vaddr_num = 1;   // The order in which arguments are passed
+    if(argptr(paddr_num, (void*) &paddr, sizeof(*paddr)) < 0) {
+        cprintf("Invalid physical address pointer received to store to.\n");
         return -1;  // If the physical address pointer (where user wants to store the final answer) don't lie withing the process address space
     }
 
-    if(argptr(1, (void *) &vaddr, sizeof(*vaddr)) < 0) {
+    if(argptr(vaddr_num, (void*) &vaddr, sizeof(*vaddr)) < 0) {
+        cprintf("Invalid virtual address pointer received to be read.\n");
         return -1;  // If the virtual address pointer don't lie withing the process address space
     }
 
-    return 0;
+    struct proc *currProcess = myproc();  // the current process which called this system call
+
+    pde_t *pgdir = currProcess->pgdir;  // The page directory of the process
+    pde_t *pde = &pgdir[PDX(vaddr)];    // Getting the page directory from the va, as done in vm.c
+
+    if(!(*pde & PTE_P)) {
+        cprintf("Page directory entry not present.\n");
+        return -1;  // If page directory entry is not present or can't be accessed in user mode
+    }
+
+    if(!(*pde & PTE_U)) {
+        cprintf("Page directory entry can't be accessed in user mode.\n");
+        return -1;  // If page directory entry is not present or can't be accessed in user mode
+    }
+
+    pde_t *pgtab = (pte_t *)P2V(PTE_ADDR(*pde));  // The page table obtained
+    pde_t *pte = &pgtab[PTX(vaddr)];              // The page table entry obtained from the va, as done in vm.c
+
+    if(!(*pte & PTE_P)) {
+        cprintf("Page table entry not present.\n");
+        return -1;  // If page table entry is not present or can't be accessed in user mode
+    }
+
+    if(!(*pte & PTE_U)) {
+        cprintf("Page table entry can't be accessed in user mode.\n");
+        return -1;  // If page table entry is not present or can't be accessed in user mode
+    }
+
+    // Concatencating last 12 bits of vaddr to the first 20 bits of the pte
+    *paddr = PTE_ADDR(*pte) | PTE_FLAGS(vaddr);
+
+    cprintf("Virtual address converted to physical address successfully.");
+    return 0;   // Indicated everything went well
 }
