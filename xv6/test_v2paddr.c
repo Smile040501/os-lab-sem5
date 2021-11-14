2e19dd29e4749f19a0cf1569f6c822a90e30ab17
Mayank Singla <singlamayank001@gmail.com>
2021-11-14 19:07:21 +0530

    Implemented test_v2paddr user program

--- xv6/test_v2paddr.c
+++ xv6/test_v2paddr.c
@@ -1,9 +1,36 @@
 #include "types.h"
 #include "user.h"
 #include "syscall.h"
+#include "memlayout.h"  // For using KERNBASE macro
+
+// Function that takes a virtual address as argument and calls v2paddr() syscall to obtain the corresponding physical address if it exists
+void getResults(addr_t *va) {
+    addr_t pa;  // Physicall address to store the final answer
+    printf(1, "Trying to convert Virtual Address: 0x%x\n", va);
+    int status = v2paddr(&pa, va);
+    if(status == 0) {   // Success
+        printf(1, "Physical Address Obtained: 0x%x\n", pa);
+    }
+    printf(1, "\n");
+}
+
+int thirdTest = 99999;  // A test global variable
 
 int main(int argc, char *argv[]) {
-    printf(1, "Test v2paddr() user program called!\n");
+
+    // First Test
+    int firstTest = 0;  // A test local variable
+    addr_t* va = (addr_t*)(&firstTest);
+    getResults(va);
+
+    // Second Test
+    addr_t* secondTest = (void*)(KERNBASE + 0x555); // A test in the Kernel space
+    va = secondTest;
+    getResults(va);
+
+    // Third Test
+    va = (addr_t*)(&thirdTest);
+    getResults(va);
 
     exit();
 }
